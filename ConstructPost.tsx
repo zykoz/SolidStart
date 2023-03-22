129b48a4a75759aa27fa41043ddb405c5b419439
zykoz <71568169+zykoz@users.noreply.github.com>
2023-03-22 03:26:07 -0400

    Update ConstructPost.tsx

--- ConstructPost.tsx
+++ ConstructPost.tsx
@@ -1,4 +1,4 @@
-import { createSignal, onMount } from "solid-js";
+import { Accessor, createMemo, createSignal, onMount } from "solid-js";
 import { marked } from "marked";
 import { markedEmoji } from "marked-emoji";
 
@@ -9,13 +9,15 @@ interface Emoji {
     [key: string]: string;
 }
 
-export default function ConstructPost(props: any) {
+export default function ConstructPost() {
     const [markdown, setMarkdown] = createSignal("");
     const [html, setHtml] = createSignal("");
 
     const [showEmojiList, setShowEmojiList] = createSignal(true);
     const [searchEmojiTitle, setSearchEmojiTitle] = createSignal("");
     const [markdownLastChar, setMardownLastChar] = createSignal("");
+
+    let unorderedListItemsMemo: Accessor<HTMLCollection> | (() => { title: string }[]);
 
     onMount(() => {
         const link = document.createElement("link");
@@ -46,7 +48,7 @@ export default function ConstructPost(props: any) {
         fetch("https://emoji-api.com/emojis?access_key=7045da4d1db20887df16cb32ad6af6a07873c4a4")
             .then((res) => res.json())
             .then((data) => {
-                const emojis = data.reduce((acc: any, { slug, character }: any) => {
+                const emojis = data.reduce((acc: any, { slug, character }: Emoji) => {
                     return { ...acc, [slug]: character };
                 }, {});
                 const options = {
@@ -57,25 +59,27 @@ export default function ConstructPost(props: any) {
                 loadEmoji(emojis);
                 document.getElementById("markdownTextArea")?.addEventListener("input", handleMarkdownChange);
             });
-    });
+    }); // End of onMount()
 
-    function handleMarkdownChange(e: any) {
-        // last to be called on mount
-        const markdownText = e.target.value;
-        setMardownLastChar(markdownText[e.target.value.length - 1]);
+    function handleMarkdownChange(e: Event) {
+        const markdownText = (e.target as HTMLTextAreaElement).value;
+        setMardownLastChar(markdownText[markdownText.length - 1]);
+
         if (markdownLastChar() === ":") {
             // toggle list add event listener
             setSearchEmojiTitle(""); // reset start with nothing
             setShowEmojiList(!showEmojiList()); // toggle the emoji list
-            showEmojiList() ? window.addEventListener("keydown", func) : window.removeEventListener("keydown", func);
+            showEmojiList() ? window.addEventListener("keydown", keyDownHandler) : window.removeEventListener("keydown", keyDownHandler);
         } else {
-            e.inputType === "deleteContentBackward"
+            (e as InputEvent).inputType === "deleteContentBackward"
                 ? setSearchEmojiTitle(searchEmojiTitle().slice(0, -1)) // delete last character emojititlesignal
                 : showEmojiList() && markdownLastChar() !== undefined && setSearchEmojiTitle(searchEmojiTitle() + markdownLastChar()); // add last character
 
-            document.querySelectorAll("#markdownEmojiList li").forEach((emoji: any) => {
-                emoji.getAttribute("title")?.toLowerCase().includes(searchEmojiTitle()) ? (emoji.style.display = "block") : (emoji.style.display = "none");
-            });
+            for (let i = 0; i < unorderedListItemsMemo().length; i++) {
+                (unorderedListItemsMemo()[i] as HTMLLIElement).getAttribute("title")?.toLowerCase().includes(searchEmojiTitle())
+                    ? ((unorderedListItemsMemo()[i] as HTMLLIElement).style.display = "block")
+                    : ((unorderedListItemsMemo()[i] as HTMLLIElement).style.display = "none");
+            }
         }
 
         setMarkdown(markdownText); // markdown text itself
@@ -85,27 +89,26 @@ export default function ConstructPost(props: any) {
 
     function loadEmoji(data: Emoji) {
         // second last in onMount
-        let index = 0;
-        const markdownEmojiList = document.getElementById("markdownEmojiList");
+        let index: number = 0;
+        const markdownEmojiList = document.getElementById("markdownEmojiList") as HTMLUListElement;
 
         for (const key in data) {
             index++;
-            let newLi = document.createElement("li");
-            let newI = document.createElement("span");
-            let newSpan = document.createElement("span");
+            let ListEl: HTMLLIElement = document.createElement("li");
+            let SpanElEmoji: HTMLSpanElement = document.createElement("span");
+            let SpanElText: HTMLSpanElement = document.createElement("span");
 
-            newLi.setAttribute("title", key);
-            newLi.setAttribute("tabindex", `${index}`); // want to be able to use tab key to go through emoji list // focus will have problems because we use an index var
-            newI.classList.add("w-[50px]", "inline-block", "text-center");
-            newI.textContent = data[key];
-            newSpan.textContent = ":" + key + ":";
-            newLi.appendChild(newI);
-            newLi.appendChild(newSpan);
+            ListEl.setAttribute("title", key);
+            ListEl.setAttribute("tabindex", `${index}`); // want to be able to use tab key to go through emoji list // focus will have problems because we use an index var
+            SpanElEmoji.classList.add("w-[50px]", "inline-block", "text-center");
+            SpanElEmoji.textContent = data[key];
+            SpanElText.textContent = ":" + key + ":";
+            ListEl.appendChild(SpanElEmoji);
+            ListEl.appendChild(SpanElText);
+            markdownEmojiList!.appendChild(ListEl);
 
-            markdownEmojiList && markdownEmojiList.appendChild(newLi);
+            unorderedListItemsMemo = createMemo(() => markdownEmojiList.children);
         }
-
-        return markdownEmojiList && markdownEmojiList.querySelectorAll("li");
     }
 
     function addTextToTextArea(textAreaElement: HTMLTextAreaElement, textToAdd: string) {
@@ -115,16 +118,15 @@ export default function ConstructPost(props: any) {
         setShowEmojiList(false); // done action so close the list
     }
 
-    const func = (e: any) => {
+    function keyDownHandler(e: KeyboardEvent) {
         let focusedIndex = 0;
-        const listItems = document.querySelectorAll("#markdownEmojiList li");
 
         if (e.key === "Tab") {
             e.preventDefault();
             const textArea = document.getElementById("markdownTextArea") as HTMLTextAreaElement;
             // Create a new input event
             const inputEvent = new Event("input", { bubbles: true });
-            setSearchEmojiTitle(":" + listItems[focusedIndex]?.title + ":");
+            setSearchEmojiTitle(":" + (unorderedListItemsMemo()[focusedIndex] as HTMLLIElement).title + ":");
             addTextToTextArea(textArea, searchEmojiTitle());
             setShowEmojiList(!showEmojiList());
             // Dispatch the input event on the text area
@@ -136,25 +138,25 @@ export default function ConstructPost(props: any) {
         if (e.key === "ArrowDown" || e.key === "Down") {
             // Move focus down
             focusedIndex++;
-            if (listItems && focusedIndex >= listItems.length) {
+            if (unorderedListItemsMemo() && focusedIndex >= unorderedListItemsMemo().length) {
                 focusedIndex = 0; // Wrap back to the top
             }
         } else if (e.key === "ArrowUp" || e.key === "Up") {
             // Move focus up
             focusedIndex--;
-            if (listItems && focusedIndex < 0) {
-                focusedIndex = listItems.length - 1; // Wrap to the bottom
+            if (unorderedListItemsMemo() && focusedIndex < 0) {
+                focusedIndex = unorderedListItemsMemo().length - 1; // Wrap to the bottom
             }
 
-            listItems.forEach((item, index) => {
-                if (index === focusedIndex) {
-                    item.classList.add("focused", "border", "border-red-700");
+            for (let i = 0; i < unorderedListItemsMemo().length; i++) {
+                if (i === focusedIndex) {
+                    (unorderedListItemsMemo()[i] as HTMLLIElement).classList.add("focused", "border", "border-red-700");
                 } else {
-                    item.classList.remove("focused", "border", "border-red-700");
+                    (unorderedListItemsMemo()[i] as HTMLLIElement).classList.remove("focused", "border", "border-red-700");
                 }
-            });
+            }
         }
-    };
+    }
 
     return (
         <>
