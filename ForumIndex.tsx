d8070eed7c6b062ca770b9982d04259ddbf5023b
zykoz <71568169+zykoz@users.noreply.github.com>
2023-04-06 04:27:26 -0400

    Create ForumIndex.tsx

---
+++ ForumIndex.tsx
@@ -0,0 +1,87 @@
+import { createServerAction$, createServerData$ } from "solid-start/server";
+import { refetchRouteData, useRouteData, RouteDataArgs } from "solid-start";
+import { db } from "~/db";
+import ForumList from "~/components/ForumList";
+import CreateTopic from "~/components/CreateTopic";
+import Folder from "~/components/Folders";
+import TopicHelper from "~/components/TopicHelper";
+import { Show, createEffect, createSignal } from "solid-js";
+import { getUserId } from "~/db/session";
+
+export function routeData({ location }: RouteDataArgs) {
+    return createServerData$(
+        async ([, page]) => {
+            const itemsPerPage = 5;
+            const forums = await db.forum.findMany({
+                take: itemsPerPage,
+                skip: itemsPerPage * (Math.max(Number(page) - 1, 0) || 0),
+                include: {
+                    topics: {
+                        select: {
+                            id: true,
+                            title: true,
+                            createdAt: true,
+                        },
+                    },
+                    createdBy: {
+                        select: {
+                            username: true,
+                        },
+                    },
+                },
+            });
+            const totalForums = await db.forum.count();
+            return { forums, totalForums, itemsPerPage };
+        },
+        { key: () => ["f", location.query["page"]] }
+    );
+}
+
+export default function ForumsPage() {
+    const [creatingForum, { Form }] = createServerAction$(async (form: FormData, { request }) => {
+        const name = form.get("forumName") as string;
+        const userId = await getUserId(request);
+
+        if (userId === null) {
+            throw new Error("User ID not found in cookie");
+        }
+
+        await db.forum.create({
+            data: {
+                name: name,
+                createdById: Number(userId),
+            },
+        });
+
+        refetchRouteData();
+    });
+    const data = useRouteData<typeof routeData>();
+    const [test, testSignal] = createSignal(data());
+    createEffect(() => {
+        console.log("data");
+        test();
+    });
+    let forumId = data.latest?.forums[0]?.id ? data.latest?.forums[0]?.id : "No_Forum";
+    return (
+        <div>
+            <Form class='w-max border border-black'>
+                <input class='dark:text-black' name='forumName' />
+                <button type='submit' disabled={creatingForum.pending}>
+                    CreateForum
+                </button>
+            </Form>
+            <Show when={creatingForum.pending}>
+                <div>Creating Edge Forum...</div>
+            </Show>
+            <Show when={creatingForum.error}>
+                <div>{creatingForum.error.message}</div>
+                <button onClick={() => creatingForum.retry()}>Retry</button>
+            </Show>
+            <CreateTopic placeholder='Create Forum' link='/create_forum' identifier={forumId} />
+
+            <TopicHelper />
+            <Folder />
+            <ForumList dataType={typeof routeData} />
+        </div>
+    );
+}
